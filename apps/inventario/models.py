c0362896ec6282fa8ccbf152dff4267ca90ddd26
Astrid Sajquim <astrid.virginia1395@gmail.com>
2018-05-09 18:29:12 -0600

    ModeloProductoLoteCompras

--- apps/inventario/models.py
+++ apps/inventario/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Producto(models.Model):
+    Id = models.AutoField(primary_key=True)
+    Nombre = models.CharField(max_length=80, null=False, blank=False)
+    Marca = models.CharField(max_length=80, null=False, blank=False)
+    Unidad = models.CharField(max_length=1, null=False, blank=False)
+    Cantidad = models.IntegerField(null=True)
+    #Cantidad hace referencia a cúantos elementos contiene, por ejemplo una caja
+    Existencia = models.IntegerField()
+    
+class Lote(models.Model):
+    Id = models.AutoField(primary_key=True)
+    PrecioVenta = models.FloatField()
+    ProductoId = models.ForeingKey('Producto', null=False, blank=False)
+
+class Compras(models.Model): #Compras a proveedor
+    Id = models.AutoField(primary_key=True)
+    PrecioVenta = models.FloatField()
+    ProductoId = models.ForeingKey('Producto', null=False, blank=False)   
