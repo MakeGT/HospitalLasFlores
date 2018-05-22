8f50bf8df86ddd9962fc51b4a214ae6b367eb78c
ACER\Acer <makepeace.gt@gmail.com>
2018-05-22 13:08:07 -0600

    Arregos

--- LasFloresHospital/settings.py
+++ LasFloresHospital/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'LasFloresHospital.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'PruebaLasFLores',
+        'NAME': 'PruebaLasFlores',
         'USER': 'postgres',
         'PASSWORD': 'admin',
         'HOST': 'localhost',
