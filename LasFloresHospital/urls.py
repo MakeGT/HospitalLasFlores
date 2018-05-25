ff743a31724a535d5101a70d50fc847991a5702d
ACER\Acer <makepeace.gt@gmail.com>
2018-05-25 11:54:21 -0600

    login

--- LasFloresHospital/urls.py
+++ LasFloresHospital/urls.py
@@ -15,9 +15,21 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth.views import login, password_reset, password_reset_done, password_reset_confirm,password_reset_complete
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('apps.inventario.urls')),
+    url(r'^inventario/', include('apps.inventario.urls')),
+    url(r'^usuarios/', include('apps.usuarios.urls')),
+    url(r'^$', login, {'template_name':'usuarios/index.html'}, name = 'login'),
+    url(r'^reset/password_reset', password_reset, {'template_name':'registro/password_reset_form.html', 
+    'email_template_name':'registro/password_reset_email.html'}, name= 'password_reset' ),
+    url(r'^reset/password_reset_done', password_reset_done,{'template_name':'registro/password_reset_done.html'},
+    name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$', password_reset_confirm, {'template_name': 'registro/password_reset_confirm.html'}, 
+    name='password_reset_confirm'),
+    url(r'^reset/done', password_reset_complete, {'template_name':'registro/password_reset_complete.html'}, 
+    name='password_reset_complete'),
+
 ]
