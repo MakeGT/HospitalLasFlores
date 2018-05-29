db6a8d7f5fbea001a2d99d45b24fb605208249db
ACER\Acer <makepeace.gt@gmail.com>
2018-05-29 15:37:48 -0600

    login_required

--- LasFloresHospital/urls.py
+++ LasFloresHospital/urls.py
@@ -15,14 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.contrib.auth.views import login, password_reset, password_reset_done, password_reset_confirm,password_reset_complete
+from django.contrib.auth.views import login, logout_then_login, password_reset, password_reset_done, password_reset_confirm,password_reset_complete
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^inventario/', include('apps.inventario.urls')),
     url(r'^usuarios/', include('apps.usuarios.urls')),
-    url(r'^$', login, {'template_name':'usuarios/index.html'}, name = 'login'),
+    url(r'^accounts/login/', login, {'template_name':'usuarios/index.html'}, name = 'login'),
+    url(r'^logout/',logout_then_login, name = 'logout'),
     url(r'^reset/password_reset', password_reset, {'template_name':'registro/password_reset_form.html', 
     'email_template_name':'registro/password_reset_email.html'}, name= 'password_reset' ),
     url(r'^reset/password_reset_done', password_reset_done,{'template_name':'registro/password_reset_done.html'},
