43e5a2576e934b0ae5fd6f597a55d7b573e231a8
Astrid Sajquim <astrid.virginia1395@gmail.com>
2018-05-30 21:38:13 -0600

    estilos

--- apps/usuarios/urls.py
+++ apps/usuarios/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
-from apps.usuarios.views import RegistroUsuario
-
+from apps.usuarios.views import RegistroUsuario, UsuariosListView
 urlpatterns =[
-    url(r'^registrar', RegistroUsuario.as_view(), name = "registrar_usuarios")
+    url(r'^registrar', RegistroUsuario.as_view(), name = "registrar_usuarios"),
+    url(r'^listar', UsuariosListView.as_view(), name = "listar_usuarios")
 ]
