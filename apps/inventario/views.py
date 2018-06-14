f6f0f9547b4a1da6b82971c5e4c35850a67b2b94
Astrid Sajquim <astrid.virginia1395@gmail.com>
2018-06-13 21:27:03 -0600

    m

--- apps/inventario/views.py
+++ apps/inventario/views.py
@@ -114,8 +114,6 @@ class PedidosCreateView(CreateView):
 
     def get_context_data(self, **kwargs):
         ctx = super(PedidosCreateView, self).get_context_data(**kwargs)
-
         ctx['productos'] = Productos.objects.all()
-           
         return ctx
 
